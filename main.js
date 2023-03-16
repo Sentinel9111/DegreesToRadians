9f3adf3f046e5b8ba6807a211d9f1af85c5719b5
Sentinel9111 <92266462+Sentinel9111@users.noreply.github.com>
2023-03-16 16:46:31 +0100

    modulo operator

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ Fijne π-dag 🙂
 */
 const π = Math.PI
 const radialenBerekenen = graden => {
-    console.log(`${graden} graden is ${graden * (π / 180)} radialen, oftewel ${graden / 180}π radialen`);
+    console.log(`\n${graden} graden is ${graden * (π / 180)} radialen, oftewel ${graden / 180}π radialen.`);
+    if (graden / 180 >= 2) {
+        console.log(`Dat is hetzelfde als ${(graden / 180) % 2}π radialen.`);
+    };
 };
-radialenBerekenen(180); // vervang 180 door het aantal graden wat je wilt omrekenen naar radialen.
+radialenBerekenen(360); // vervang dit getal door het aantal graden wat je wilt omrekenen naar radialen.
