544903d2ff6e4d2a03f4cebfeafb7811c801a8c2
Sentinel9111 <92266462+Sentinel9111@users.noreply.github.com>
2023-03-16 16:03:41 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+/*
+Fijne π-dag 🙂
+*/
+const π = Math.PI
+const radialenBerekenen = graden => {
+    console.log(graden, 'graden is', graden * (π / 180) / 1 , 'radialen');
+};
+radialenBerekenen(180); // vervang 180 door het aantal graden wat je wilt omrekenen naar radialen.
